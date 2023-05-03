63fd9cf9b0f9a4d3e0bdfdcd05841c4237159f1d
Giray VARDAL <gvardal@yahoo.com>
2023-05-03 14:50:25 +0300

    Add Proje Durumu ve Olay Türü

---
+++ Source/Library/ScientaScheduler.Library/Constants/OlayTuru.cs
@@ -0,0 +1,25 @@
+﻿namespace ScientaScheduler.Library.Constants
+{
+    public class OlayTuru
+    {
+        public Dictionary<int, string>  OlayTur = new();
+
+        public OlayTuru()
+        {
+            OlayTur.Clear();
+            OlayTur.Add(1, "Görev");
+            OlayTur.Add(2, "Randevu");
+            OlayTur.Add(3, "Araç Talebi");
+            OlayTur.Add(4, "DÖF");
+            OlayTur.Add(5, "Toplantı Faaliyeti");
+            OlayTur.Add(6, "Proje Faaliyeti");
+            OlayTur.Add(7, "İstek");
+            OlayTur.Add(8, "Şikayet");
+            OlayTur.Add(9, "Taşeron");
+            OlayTur.Add(10, "Kilometretaşı");
+            OlayTur.Add(11, "Çoklu Görev");
+        }
+
+
+    }
+}
---
+++ Source/Library/ScientaScheduler.Library/Constants/ProjeDurum.cs
@@ -0,0 +1,22 @@
+﻿namespace ScientaScheduler.Library.Constants
+{
+    public class ProjeDurum
+    {
+        public Dictionary<int, string> Durum = new();
+
+        public ProjeDurum()
+        {
+            Durum.Clear();
+            Durum.Add(0, "Onayda");
+            Durum.Add(1, "Red Edildi");
+            Durum.Add(2, "Kontrol");
+            Durum.Add(3, "Okunacak");
+            Durum.Add(4, "Başladı");
+            Durum.Add(5, "Durduruldu");
+            Durum.Add(7, "Ertelendi");
+            Durum.Add(6, "Bekliyor");
+            Durum.Add(8, "Tamamlandı");
+            Durum.Add(9, "İptal Edildi");
+        }
+    }
+}
